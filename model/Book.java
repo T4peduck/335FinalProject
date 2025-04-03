49ee73fed200984af32c4b35bb62b1fffb080d6e
T4peduck <jthill1@arizona.edu>
2025-04-03 11:02:09 -0700

    adding more to book.java as to get rdy for pulling the textFile

--- model/Book.java
+++ model/Book.java
@@ -3,6 +3,9 @@ public class Book {
 	private String title;
 	private String author;
 	private String callNumber;
+
+    private String summary;
+    private String file;
 	
     public Book(String title, String author, String callNumber) {
         this.title = title;
