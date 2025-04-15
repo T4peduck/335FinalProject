4e9cea20e823ee482f6952dc4769daa2f5cfe232
lix2_an_wei <931115lilian@gmail.com>
2025-04-15 14:38:41 -0700

    add hashPassword method

--- model/User.java
+++ model/User.java
@@ -1,13 +1,39 @@
 package model;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.ArrayList;
+
 
 public class User {
     private String username;
     private String password;
+    private byte[] salted;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+    
+    public void hashPassword() throws NoSuchAlgorithmException {
+    	try {
+    		byte[] salt;
+        	SecureRandom saltGen = new SecureRandom();
+            salt = new byte[2];
+            saltGen.nextBytes(salt);
+            MessageDigest hashFunct = MessageDigest.getInstance("MD5");
+            salted = new byte[2 + password.length()];
+            for (int i = 0; i < 2; i++)
+                salted[i] = salt[i];
+
+            for (int i = 2; i < 2 + password.length(); i++)
+                salted[i] = password.getBytes()[i - 2];
+    	}
+    	
+    	catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    	
     }
 
     public ArrayList<Book> searchBook(String name, String author) {
---
+++ view/Main.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.util.HashMap;
+
+public class Main {
+	private static HashMap<String, User> users;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
