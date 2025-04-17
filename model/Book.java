55b6048b7d7c98340f4b3d7bfffeb438a11b8afd
Marissa Kroeger <kroegerm2@arizona.edu>
2025-04-17 10:27:26 -0700

    updates to library and Borrower stubs for interacting with the library. Some changes from merging

--- model/Book.java
+++ model/Book.java
@@ -18,8 +18,7 @@ public class Book {
 		if (getClass() != obj.getClass())
 			return false;
 		Book other = (Book) obj;
-		return Objects.equals(author, other.author)
-				&& Objects.equals(title, other.title);
+		return Objects.equals(author, other.author) && Objects.equals(title, other.title);
 	}
 
 	public final String title;
@@ -27,45 +26,55 @@ public class Book {
 
 	public final String summary;
 	public final String filePath;
-	
-    public Book(String title, String author, 
-    			String summary, String filePath) {
-        this.title = title;
-        this.author = author;
-        this.summary = summary;
-        this.filePath = filePath;
-    }
-	
+
+	public Book(String title, String author, String summary, String filePath) {
+		this.title = title;
+		this.author = author;
+		this.summary = summary;
+		this.filePath = filePath;
+	}
+
 	public static Comparator<Book> authorFirstComparator() {
 		return new Comparator<Book>() {
 			public int compare(Book book1, Book book2) {
-				
+
 				int comp = book1.author.compareTo(book2.author);
-				
+
 				if (comp != 0) {
 					return comp;
 				}
-				
+
 				return book1.title.compareTo(book2.title);
 			}
 		};
 	}
-	
+
 	public static Comparator<Book> titleFirstComparator() {
 		return new Comparator<Book>() {
 			public int compare(Book book1, Book book2) {
-				
+
 				int comp = book1.title.compareTo(book2.title);
-				
+
 				if (comp != 0) {
 					return comp;
 				}
-				
+
 				return book1.author.compareTo(book2.author);
 			}
 		};
 	}
-    
-   
-    
+
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getSummary() {
+		return summary;
+	}
+
 }
--- model/Borrower.java
+++ model/Borrower.java
@@ -1,21 +1,56 @@
 package model;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Borrower extends User {
+	private HashMap<String, ArrayList<Book>> history;
+	private HashMap<String, ArrayList<Book>> historyByAuthor;
+	private HashMap<String, ArrayList<Book>> historyByGenre;
+	
+	private ArrayList<Book> checkedOut;
+	private ArrayList<Book> onHold;
+	
     public Borrower (String username, String password) throws NoSuchAlgorithmException {
         super(username, password);
+        history = new HashMap<>();
+        historyByAuthor = new HashMap<>();
+        historyByGenre = new HashMap<>();
+        checkedOut = new ArrayList<>();
     }
     
     public Borrower (Borrower borrower) {
     	super(borrower);
     }
 
-    public Book checkOutBook(String id) {
+    public void checkOutBook(String id) {
+    	// use the library method for checking out
+    	
+    	// update checkedOut
         return null;
+    }
+    
+    public void checkinBook() {
+    	// use library method to check in
+    	
+    	// update histories and cheked out
+    	
+    	// automatically check out next book on hold if available??
     }
 
-    public Book putBookOnHold(String id) {
+    public void putBookOnHold(String id) {
+    	// use the library method for holding?
         return null;
     }
+    
+    public ArrayList<Book> getRecommendations(){
+    	// find their most common genre from history
+    	
+    	// call a library method for getting books from this genre
+    }
+    
+    // geters? History with the different comparators, searching history
+    
+    // calling library methods they have access to: searches, etc
 }
