ff5442101b9d236d0a4eebaaa885cf1da109ce8b
T4peduck <05jthill@gmail.com>
2025-04-24 13:21:23 -0700

    idk

---
+++ model/DataController.java
@@ -0,0 +1,16 @@
+package model;
+
+
+/*
+ * Saves the Library Data to a text file so data persist
+ * between uses
+ */
+public class DataController {
+	private static String FILEPATH = "BookData";
+	
+	public static void saveBookData() {
+		
+	}
+	
+	
+}
