1b8a5e545cd911ddc7dfcd815fc7bf214b28be84
lix2_an_wei <931115lilian@gmail.com>
2025-04-24 05:42:48 -0700

    Add Junit test stubs

---
+++ test/LibrarianTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LibrarianTest {
+
+	@Test
+	void testRemoveBorrower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRecommend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveRecommend() {
+		fail("Not yet implemented");
+	}
+
+}
