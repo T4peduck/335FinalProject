78a8f8fa51a66a37eb27ee6fa281ce57327428d1
lix2_an_wei <931115lilian@gmail.com>
2025-04-24 04:40:40 -0700

    Update Librarian methods

--- model/Librarian.java
+++ model/Librarian.java
@@ -4,39 +4,75 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Librarian extends User{
-    public Librarian(String username, String password) throws NoSuchAlgorithmException {
+	private ArrayList<Borrower> borrowers;
+    public Librarian(String username, String password, ArrayList<Borrower> borrowers) throws NoSuchAlgorithmException {
         super(username, password);
+        // The borrowers are assumed to be passed in by View
+        this.borrowers = borrowers;
     }
     
     public Librarian (Librarian librarian) {
     	super(librarian);
+    	borrowers = librarian.getBorrowerList();
     }
 
-    public ArrayList<String> getBorrowerList() {
-        return null;
+    // This won't allow any access to the Borrowers
+    public ArrayList<Borrower> getBorrowerList() {
+    	ArrayList<Borrower> borrowerList = new ArrayList<>();
+    	for (Borrower borrower : borrowers)
+    		borrowerList.add(new Borrower(borrower));
+    	
+        return borrowerList;
     }
 
     public Borrower inspectBorrower() {
         return null;
     }
 
-    public void removeBorrower() {
-
+    /*
+     * @pre - Borrower borrower is an existing user
+     */
+    public void removeBorrower(Borrower borrower) {
+    	borrowers.remove(borrower);
     }
     
-    public void addBook() {
+    public void addBook(Book book, Library library) {
+    	library.addBook(book);
+    }
+    
+    public void removeBook(Book book, Library library) {
+    	library.removeBook(book);
+    }
+    
+    public ArrayList<String> getAvailBookListByAuthor(Library library) {
+    	ArrayList<String> bookList = new ArrayList<>();
+    	for (Book book : library.getAvailBooksByAuthor())
+    		bookList.add(book.toString());
     	
+        return bookList;
     }
     
-    public void removeBook() {
+    public ArrayList<String> getAvailBookListByTitle(Library library) {
+    	ArrayList<String> bookList = new ArrayList<>();
+    	for (Book book : library.getAvailBooksByTitle())
+    		bookList.add(book.toString());
     	
+        return bookList;
     }
     
-    public ArrayList<String> getAvailBookList() {
-        return null;
+    public ArrayList<String> getUnavailBookListByAuthor(Library library) {
+    	ArrayList<String> bookList = new ArrayList<>();
+    	for (Book book : library.getUnvailBooksByAuthor())
+    		bookList.add(book.toString());
+    	
+        return bookList;
     }
-
-    public ArrayList<String> getUnavailBookList() {
-        return null;
+    
+    public ArrayList<String> getUnavailBookListByTitle(Library library) {
+    	ArrayList<String> bookList = new ArrayList<>();
+    	for (Book book : library.getUnvailBooksByTitle())
+    		bookList.add(book.toString());
+    	
+        return bookList;
     }
 }
