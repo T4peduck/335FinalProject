ee1c3d96d96b4e609413735ce34f9f9c836032c3
lix2_an_wei <931115lilian@gmail.com>
2025-04-24 09:45:28 -0700

    Update UserTest.java

--- test/UserTest.java
+++ test/UserTest.java
@@ -2,12 +2,37 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.jupiter.api.Test;
 
+import model.User;
+
 class UserTest {
+	
+	@Test
+	void testSearchBookByTitle() throws NoSuchAlgorithmException {
+		User u1 = new User("lix2_an_wei", "1a2B3c!");
+		fail("Not yet implemented");
+	}
 
 	@Test
-	void test() {
+	void testSearchBookByAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckAvailable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPasswordMatched() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPassword() {
 		fail("Not yet implemented");
 	}
 
