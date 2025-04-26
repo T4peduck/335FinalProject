0dae9336e25e36356b4dcb4f9b0bfa4aa3b88dba
lix2_an_wei <931115lilian@gmail.com>
2025-04-25 20:58:07 -0700

    Comment User.java

--- model/User.java
+++ model/User.java
@@ -24,38 +24,65 @@ public class User {
     
     /*
 	 * public ArrayList<Book> searchBookByTitle(String name, Library library) -
-	 * 		returns an arraylist of all books written by the specified author
+	 * 		returns an arraylist of all books with the specified title
 	 */
 	public ArrayList<Book> searchBookByTitle(String name, Library library) {
         return library.searchAllBooksByTitle(name);
     }
 	
+	/*
+	 * public ArrayList<Book> searchBookByAuthor(String name, Library library) -
+	 * 		returns an arraylist of all books written by the specified author
+	 */
 	public ArrayList<Book> searchBookByAuthor(String author, Library library) {
         return library.searchAllBooksByAuthor(author);
     }
 
+	/*
+	 * public boolean checkAvailable(String id, Library library) -
+	 * 		determines if the Book with the id is available in library
+	 */
     public boolean checkAvailable(String id, Library library) {
         return library.searchAvailBookByID(id) != null;
     }
     
+    /*
+	 * public boolean passwordMatched(String password) -
+	 * 		determines if the password matches the User's password
+	 */
     public boolean passwordMatched(String password) throws NoSuchAlgorithmException {
     	return Arrays.equals(getHashPassword(password, salt), this.password);
     }
     
+    /*
+	 * public String getUserName() -
+	 * 		returns the username of User
+	 */
     public String getUserName() {
     	return username;
     }
     
+    /*
+	 * public byte[] getPassword() -
+	 * 		returns the hashed password in bytes
+	 */
     public byte[] getPassword() {
 		return password.clone();
 	}
 
+    /*
+	 * public byte[] getSalt() -
+	 * 		returns the salt used for cryptographic hash
+	 */
 	public byte[] getSalt() {
 		return salt.clone();
 	}
     
 	/*
      * @pre - password.matches("(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*\\d.*)(?=.*[~!@#$%^&*()_+].*)(?!.*\\s.*).+")
+     * 
+     * public void setPassword(String password) -
+	 * 		sets the initial password to the User or reset the password
      */
 	public void setPassword(String password) throws NoSuchAlgorithmException {
     	SecureRandom saltGen = new SecureRandom();
@@ -64,6 +91,12 @@ public class User {
         this.password = getHashPassword(password, salt);
     }
         
+	/*
+     * @pre - salt is the same one that hashes the password
+     * 
+     * private byte[] getHashPassword(String password, byte[] salt) -
+	 * 		generates and returns the hashed password in bytes
+     */
 	private byte[] getHashPassword(String password, byte[] salt) throws NoSuchAlgorithmException {
 		MessageDigest hashFunct = MessageDigest.getInstance("MD5");
 		byte[] digest = new byte[2 + password.length()];
